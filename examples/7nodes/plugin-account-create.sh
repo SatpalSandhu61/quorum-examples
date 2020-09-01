c04a311ffd418d74d136b3b8c1560cbc25811942
SatpalSandhu61 <quorum@satpal.co.uk>
2020-09-01 16:11:16 +0100

    Adding config file for geth plugin, also scripts to create/import accounts into plugin.

---
+++ examples/7nodes/plugin-account-create.sh
@@ -0,0 +1,4 @@
+export HASHICORP_SECRET_ID=5b010f4b-c103-ba75-0385-4c52c3ce47fc
+export HASHICORP_ROLE_ID=2dd6c6d3-202c-606b-a6bb-0d9e0f65acdb
+
+geth account plugin new --plugins file://./geth-plugin-settings.json --plugins.account.config file://./newacct.json --plugins.skipverify
