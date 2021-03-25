4f500b8efe66c2c30508b131c08cddd2c93463be
SatpalSandhu61 <quorum@satpal.co.uk>
2021-03-25 11:36:00 +0000

    Merge from origin

--- examples/7nodes/test-generate-data.js
+++
@@ -1,9 +0,0 @@
-//Generate the bytecode for an abi function call
-
-var web3 = require('web3');
-
-web3= new web3();
-fromAddr = '0xed9d02e382b34818e88b88a309c7fe71e65f419d';
-
-var data = web3.eth.abi.encodeFunctionCall({ name: 'setVal', type: 'function',inputs: [{type: 'uint256',name: 'x'}]}, ['15']);
-console.log(data);
